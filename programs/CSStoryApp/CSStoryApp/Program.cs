a6f424c1a726ae835bef0efb1831d7cd9bdf1c33
David Bergeron <dbergeron2@nait.ca>
2018-09-07 11:45:27 -0600

    Initial commit of first two Review Exercises

---
+++ programs/CSStoryApp/CSStoryApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSStoryApp
+{
+    class CSStory 
+    {
+        static void Main(string[] args)
+        {
+            string firstName; // User's first name
+
+            Console.WriteLine();
+            Console.WriteLine("Electronic messages use the symbol :-) to indicate a funny comment.\n" +
+                              "The :-) symbol is called a 'smiley'. Look at it sideways and you'll\n" +
+                              "see why.");
+
+
+            /* The story of C# */
+            Console.WriteLine("The name C# was inpired by musical notation where a sharp indicates\n" +
+                              "that the written note should be made a semitone higher in pitch.\n" +
+                              "This is similar to the language name C++, where \"++\" indicates that a\n" +
+                              "variable should be incremented by 1. The sharp symbol also resembles a \n" +
+                              "ligature for four \"+\" symbols (in a two-by-two grid), further \n" +
+                              "implying that the language is an increment of C++.");
+
+            Console.WriteLine();
+            Console.WriteLine("Due to technical limitations to display and the fact that the sharp\n" +
+                              "symbol is not present on the standard keyboard, the number sign (#)\n" +
+                              "was chosen to represent the sharp symbol in the written name of the\n" +
+                              "programming language.");
+
+            /* Get User's name and display Personalized Message */
+            Console.WriteLine();
+            Console.Write("Enter your first name: ");
+            firstName = Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("When you've finished this course, and you are NEW AND IMPROVED,\n " +
+                              "people may call you " + firstName + "#. :-)");
+
+                   }
+    }
+}
---
+++ programs/GuessAge/GuessAge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessAgeApp
+{
+    class GuessAge
+    {
+        static void Main(string[] args)
+        {
+            string inString; // for key input val;
+            int num;
+
+            // Explain how to compute a "magic" number.
+            Console.WriteLine("I can guess your age from a magic number, here's how:");
+            Console.WriteLine("  1. Add 21 to your age.");
+            Console.WriteLine("  2. Double the result of step 1.");
+            Console.WriteLine("  3. Add your age to the result of step 2.");
+            Console.WriteLine("  4. Subtract 18 from the result of step 3.");
+        }
+    }
+}
