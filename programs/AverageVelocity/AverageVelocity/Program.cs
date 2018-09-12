f42ae1fc2f861a8d55b7f3a2dedeb772567c5a43
David Bergeron <dbergeron2@nait.ca>
2018-09-12 10:51:30 -0600

    Daily commit

---
+++ programs/AverageVelocity/AverageVelocity/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AverageVelocity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1, x2, t1, t2, avgVel;
+
+            Console.WriteLine("Enter points x1 and x2: ");
+            x1 = double.Parse(Console.ReadLine());
+            x2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter times t1 and t2: ");
+            t1 = double.Parse(Console.ReadLine());
+            t2 = double.Parse(Console.ReadLine());
+
+            avgVel = (x2 - x1) / (t2 - t1);
+            Console.WriteLine($"The average velocity is: {avgVel}");
+        }
+    }
+}
---
+++ programs/TakeHeart/TakeHeart/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TakeHeart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int magicNum = 220;
+            int age, maxHeartRate;
+            float lowLimit, highLimit;
+
+            Console.Write("Enter your age: ");
+            age = int.Parse(Console.ReadLine());
+            maxHeartRate = magicNum - age;
+
+            lowLimit = (float) 0.6 * maxHeartRate;
+            highLimit = (float)0.75 * maxHeartRate;
+
+            Console.WriteLine($"THRZ: {lowLimit} - {highLimit} BPM");
+        }
+    }
+}
