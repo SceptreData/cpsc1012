d76f48cfa63bd561bcec2837255b87899238272b
David Bergeron <dbergeron2@nait.ca>
2018-09-19 10:49:17 -0600

    added todays files + core portfolio.

---
+++ programs/CorePortfolio1/CorePortfolio1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CorePortfolio1
+{  
+    /*
+     * Purpose: Create a costed Packing Slip refelecting the materials needed to build
+     *          a playground fence.
+     *          
+     * Input: Rectangular Playground dimensions (in Feet):
+     *          - Length
+     *          - Width
+     *          - Height
+     *        Gate Dimensions
+     *          - Width
+     *          - Height
+     *        Distance between Posts (in Feet)
+     *        Type of Paint/Stain
+     *        Type of fence material
+     *         
+     * Process:  <!!! TODO !!!>
+     * 
+     * 
+     * 
+     * Output:  Amount of Fencing Material required and Extended Price
+     *          Number of Posts required and extended price
+     *          Lineal feet of railing required
+     *          Amount of paint required and extended price
+     *          Number of gates required and extended price
+     *          Net Price
+     *          Total GST (5%)
+     *          Total Price
+     *          
+     * Written By: David Bergeron
+     * Date Modified: 2018.09.17
+     * */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ programs/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double bmi;
+            Console.WriteLine("Enter BMI Value: ");
+            bmi = double.Parse(Console.ReadLine());
+
+            string bmiString = "";
+            //bmi   meaning
+            if (bmi < 18.5)
+                bmiString = "Underweight";
+            else if (bmi >= 18.5 && bmi < 25)
+                bmiString = "normal";
+            else if (bmi >= 25 && bmi < 30)
+                bmiString = "overweight";
+            else if (bmi > 30)
+                bmiString = "Obese";
+
+            Console.WriteLine("User is: " + bmiString);
+        }
+    }
+}
