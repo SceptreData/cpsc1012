98f685cbefa214d262dafa0310e379f3aa3c9c49
David Bergeron <dbergeron2@nait.ca>
2018-09-24 11:53:43 -0600

    added todays lesson

---
+++ programs/CPSC1012-Ex04-DavidBergeron/CPSC1012-Ex04-DavidBergeron/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CPSC1012_Ex04_DavidBergeron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a year: ");
+            int year = int.Parse(Console.ReadLine());
+            bool isLeapYear = false;
+
+            // if year is divisible by 4
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            {
+                isLeapYear = true;
+            }
+
+            if (isLeapYear)
+            {
+                Console.WriteLine($"{year} is a leap year!");
+            } else {
+                Console.WriteLine($"{year} is NOT a leap year.");
+            }
+        }
+    }
+}
