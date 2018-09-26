bfc5f50574da1f166cec80bdb6a3eb8362ab794c
David Bergeron <david.f.bergeron@gmail.com>
2018-09-26 12:48:52 -0600

    Did some of the math!

--- programs/CorePortfolio1/CorePortfolio1/Program.cs
+++ programs/CorePortfolio1/CorePortfolio1/Program.cs
@@ -53,24 +53,62 @@ namespace CorePortfolio1
             const double GATE_FIXED_PRICE = 120;
             const double GATE_SQFT_PRICE = 15.75;
 
+            const WASTE_FACTOR = 0.10;
+
             // Declare inputs.
             double fenceLength;
             double fenceWidth;
             double fenceHeight;
-
-            double postDistance; // Distance between posts
+            
+            double postDistance; 
 
             double gateWidth;
             double gateHeight;
 
             // Declare Process variables (Stuff we need to calculate)
-            double 
-            double
-            double
-            double
-            double
-            double
+            double fencePerimeter;
+            double amountCedar;
 
+            double fenceMaterial;
+            double gateMaterial;
+
+            double numPosts;
+            double feetOfRailing;
+            double numPaintBuckets;
+
+            double subTotal;
+            double taxAmount;
+            double totalCost;
+
+            // Grab our inputs
+            Console.Write("Enter the width of the playground\t>> ");
+            fenceWidth = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the length of the playground\t>> ");
+            fenceLength = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the height of the fence\t\t>> ");
+            fenceHeight = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the width of the gate\t>> ");
+            gateWidth = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the height of the gate\t\t>> ");
+            gateHeight = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the space between posts\t\t>> ");
+            postDistance = double.Parse(Console.ReadLine());
+
+            // Let's do some math.
+            // First let's figure out how big the gate is.
+            fencePerimeter = (fenceLength * 2) + (fenceWidth * 2) - gateWidth;
+            amountCedar = fencePerimeter * fenceHeight;
+
+            // TODO: Add Math.Round?
+            numPosts = Math.Ceiling( (fencePerimeter / postDistance) + 1 );
+
+            feetOfRailing = fencePerimeter * 2;
+            numPaintBuckets = Math.Ceiling(amountCedar / 400);
         }
     }
 }
