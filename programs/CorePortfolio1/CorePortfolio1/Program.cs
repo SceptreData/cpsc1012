b38f854670490e7d4878a270e43549fa75d4a4e0
David Bergeron <dbergeron2@nait.ca>
2018-09-28 11:43:16 -0600

    Finished Core Portfolio

--- programs/CorePortfolio1/CorePortfolio1/Program.cs
+++ programs/CorePortfolio1/CorePortfolio1/Program.cs
@@ -41,10 +41,6 @@ namespace CorePortfolio1
     {
         static void Main(string[] args)
         {
-            /* QUESTIONS FOR PROF 
-             * How to calculate waste for paint?
-             */
-
             // Declare my Constants
             const double WASTE_FACTOR = 1.10;
 
@@ -59,7 +55,6 @@ namespace CorePortfolio1
             const double GATE_PRICE_PER_SQFT = 15.75;
 
             const double TAX_RATE = 0.05;           
-
 
             // Declare inputs.
             double fenceLength;
@@ -89,7 +84,6 @@ namespace CorePortfolio1
             double numPaintBuckets;
             double paintCost;
 
-            double gateSqFt;
             double gateMaterial;
             double gateCost;
 
@@ -98,49 +92,39 @@ namespace CorePortfolio1
             double totalPrice;
 
             // Grab our inputs
-            //Console.Write("Enter the width of the playground\t>> ");
-            //fenceWidth = double.Parse(Console.ReadLine());
+            Console.Write("Enter the width of the playground\t>> ");
+            fenceWidth = double.Parse(Console.ReadLine());
 
-            //Console.Write("Enter the length of the playground\t>> ");
-            //fenceLength = double.Parse(Console.ReadLine());
+            Console.Write("Enter the length of the playground\t>> ");
+            fenceLength = double.Parse(Console.ReadLine());
 
-            //Console.Write("Enter the height of the fence\t\t>> ");
-            //fenceHeight = double.Parse(Console.ReadLine());
+            Console.Write("Enter the height of the fence\t\t>> ");
+            fenceHeight = double.Parse(Console.ReadLine());
 
-            //Console.Write("Enter the width of the gate\t\t>> ");
-            //gateWidth = double.Parse(Console.ReadLine());
+            Console.Write("Enter the width of the gate\t\t>> ");
+            gateWidth = double.Parse(Console.ReadLine());
 
-            //Console.Write("Enter the height of the gate\t\t>> ");
-            //gateHeight = double.Parse(Console.ReadLine());
+            Console.Write("Enter the height of the gate\t\t>> ");
+            gateHeight = double.Parse(Console.ReadLine());
 
-            //Console.Write("Enter the space between posts\t\t>> ");
-            //postDistance = double.Parse(Console.ReadLine());
-            fenceWidth = 30;
-            fenceLength = 30;
-            fenceHeight = 6;
-            gateWidth = 4;
-            gateHeight = 4;
-            postDistance = 6;
+            Console.Write("Enter the space between posts\t\t>> ");
+            postDistance = double.Parse(Console.ReadLine());
 
-            
 
-            /* FENCING MATERIAL CALCULATION */
-            // First lets calculate the total perimeter of our playground.
+            /* FENCING MATERIAL CALCULATION
+             * We calculate our total fencingMaterial by:
+             *    - Calculating the total perimeter of the playground
+             *    - Determining perimeter of the fence by subtracting the width of the gate.
+             *    - calculating Square Footage of our Fence (fencePerimeter * fenceHeight)
+             *    - Making sure to factor in our waste ratio. */
             totalPerimeter = (fenceLength * 2) + (fenceWidth * 2);
-
-            // To get the perimeter of our fence, we remove the width of the gate from the
-            // total perimeter.
             fencePerimeter = totalPerimeter - gateWidth;
 
-            // We can now calculate the square footage of fence material that we need by
-            // multiplying the linear length (the perimeter) by the height.
             fenceSqFt = fencePerimeter * fenceHeight;
 
-            // Now that we have our precise measurement, we need to factor in the waste
-            // calculation.
-            fenceMaterial = fenceSqFt * WASTE_FACTOR;
+            fenceMaterial = Math.Ceiling(fenceSqFt * WASTE_FACTOR);
 
-            // Now that we know how much fenceMaterial we have, we can calculate the price.
+            // Now that we know how much fenceMaterial we need, we can calculate the price.
             fenceCost = fenceMaterial * FENCE_PRICE_PER_SQFT;
 
             /* POST CALCULATIONS 
@@ -148,7 +132,7 @@ namespace CorePortfolio1
              *   - dividing the lineal footage of the fence by the distance between posts.
              *   - Rounding up the nearest whole number (using Math.Ceiling)
              *   - Adding an extra post for the first corner. */
-            numPosts = Math.Ceiling((fencePerimeter / postDistance)) + 1;
+            numPosts = Math.Ceiling((totalPerimeter / postDistance)) + 1;
 
             // Calculate the net cost for our posts.
             postCost = numPosts * POST_PRICE;
@@ -157,7 +141,8 @@ namespace CorePortfolio1
              * Calculate how many feet of Railing we need by:
              *   - multiplying our fence perimeter by 2 (for the top rail and the bottom rail)
              *   - multiply our linear feet of railing by the waste factor. */
-            feetOfRailing = fencePerimeter * 2;
+            //feetOfRailing = fencePerimeter * 2;
+            feetOfRailing = Math.Ceiling(totalPerimeter * 2);
             railMaterial = feetOfRailing * WASTE_FACTOR;
 
             // Calculate net cost of our railing.
@@ -166,13 +151,14 @@ namespace CorePortfolio1
             /* PAINT CALCULATIONS
              * Calculate how much paint we need by:
              *      - divide the Square Footage of fence we need to paint by 100.
+             *         - Need to paint both sides of the fence, so double our fence SquareFootage.
              *         - Each gallon of paint covers 400 SQFT of fence.
              *         - There are 4 Quarts of paint per gallon, so each quart of paint
              *           covers 100 Square Feet of Fence
              *      - Making sure to include our WASTE_FACTOR
              *      - Rounding up to the nearest whole number (We can't buy half a bucket of paint) */
-            quartsOfPaint = (fenceSqFt / SQFT_PAINT_PER_QUART);
-            numPaintBuckets = Math.Ceiling( quartsOfPaint * WASTE_FACTOR );
+            quartsOfPaint = (fenceSqFt * 2) / SQFT_PAINT_PER_QUART;
+            numPaintBuckets = Math.Ceiling( quartsOfPaint );
 
             // Calculate net cost of our paint.
             paintCost = numPaintBuckets * PAINT_PRICE_PER_BUCKET;
@@ -181,8 +167,7 @@ namespace CorePortfolio1
              *  Calculate how much gate material we need by:
              *     - determining Square Footage of our gate (gateWidth * gateHeight) 
              *     - Making sure to include our WASTE_FACTOR */
-            gateSqFt = gateWidth * gateHeight;
-            gateMaterial = gateSqFt * WASTE_FACTOR;
+            gateMaterial = gateWidth * gateHeight;
 
             // Calculate Net cost for the gate, including fixed cost.
             gateCost = GATE_FIXED_PRICE + (gateMaterial * GATE_PRICE_PER_SQFT);
@@ -196,55 +181,55 @@ namespace CorePortfolio1
 
             /* PRINT THOSE RESULTS */
             Console.WriteLine("\nInvoice and Packing Slip\n");
-            Console.WriteLine(String.Format("  {0,-8:0.0} {1,5} {2,-20} @ {3,8:0.00} = {4,12:C}",
+            Console.WriteLine("  {0,-8:F0} {1,5} {2,-20} @ {3,8:0.00} = {4,12:C}",
                                              fenceMaterial,
                                              "^ft.",
                                              "Fence Material",
                                              FENCE_PRICE_PER_SQFT,
                                              fenceCost
-                                             ));
-            Console.WriteLine(String.Format("  {0,-8:0.0} {1,5} {2,-20} @ {3,8:0.00} = {4,12:C}",
+                                             );
+            Console.WriteLine("  {0,-8:F0} {1,5} {2,-20} @ {3,8:0.00} = {4,12:C}",
                                              numPosts,
                                              "",
                                              "Posts",
                                              POST_PRICE,
                                              postCost
-                                             ));
-            Console.WriteLine(String.Format("  {0,-8:0.0} {1,5} {2,-20} @ {3,8:0.00} = {4,12:C}",
+                                             );
+            Console.WriteLine("  {0,-8:F0} {1,5} {2,-20} @ {3,8:0.00} = {4,12:C}",
                                              railMaterial,
                                              "ft.",
                                              "Railing",
                                              RAIL_PRICE_PER_FOOT,
                                              railCost 
-                                             ));
-            Console.WriteLine(String.Format("  {0,-8:0.0} {1,5} {2,-20} @ {3,8} = {4,12:C}",
+                                             );
+            Console.WriteLine("  {0,-8:F0} {1,5} {2,-20} @ {3,8} = {4,12:C}",
                                              1.0,
                                              "",
                                              "Gate",
                                              "",
                                              gateCost
-                                             ));
-            Console.WriteLine(String.Format("  {0,-8:0.0} {1,5} {2,-20} @ {3,8:0.00} = {4,12:C}",
+                                             );
+            Console.WriteLine("  {0,-8:F0} {1,5} {2,-20} @ {3,8:0.00} = {4,12:C}",
                                              numPaintBuckets,
                                              "qts.",
                                              "Paint",
                                              PAINT_PRICE_PER_BUCKET,
                                              paintCost
-                                             ));
+                                             );
 
             // Print our Totals
-            Console.WriteLine(String.Format("\n{0,-35} {1, -10}   = {2,12}",
+            Console.WriteLine("\n{0,-35} {1, -10}   = {2,12:C}",
                                             " ",
                                             "Net Price",
-                                            netPrice));
-            Console.WriteLine(String.Format("{0, -35} {1, -10}   = {2,12}",
+                                            netPrice);
+            Console.WriteLine("{0, -35} {1, -10}   = {2,12:C}",
                                             " ",
                                             "GST",
-                                            taxAmount));
-            Console.WriteLine(String.Format("{0, -35} {1, -10}   = {2,12}",
+                                            taxAmount);
+            Console.WriteLine("{0, -35} {1, -10}   = {2,12:C}",
                                             " ",
                                             "Total",
-                                            totalPrice));
+                                            totalPrice);
         }
     }
 }
