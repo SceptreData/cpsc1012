47d03937b87e630a11fc7f4c680b666001c674ce
David Bergeron <dbergeron2@nait.ca>
2018-10-26 11:37:40 -0600

    Pushing todays programs

---
+++ programs/AnyalyzeNumbersMethod/AnyalyzeNumbersMethod/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace AnalyzeNumbers
+{
+    // This program prompt for the number of items
+    // and then each in each item and calculate the average
+    // of all the items
+    class Program
+    {
+
+        static double[] GetItems()
+        {
+            Console.Write("Enter the number of items: ");
+            int size = int.Parse(Console.ReadLine());
+
+            double[] arr = new double[size];
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = double.Parse(Console.ReadLine());
+            }
+
+            return arr;
+        }
+
+        static double AverageValue(double[] numArray)
+        {
+            double sum = 0;
+            foreach(double num in numArray)
+            {
+                sum += num;
+            }
+            return sum / numArray.Length;
+        }
+
+        static void DisplayArrayElements(double[] numArray)
+        {
+            Console.WriteLine("Number List:");
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                Console.Write($"{numArray[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        static void ShuffleArrayElements(double[] arr)
+        {
+            Random r = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int randIdx = r.Next(arr.Length);
+                double tmp = arr[i];
+                arr[i] = arr[randIdx];
+                arr[randIdx] = tmp;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            double[] numbers = GetItems();
+            DisplayArrayElements(numbers);
+
+            ShuffleArrayElements(numbers);
+            DisplayArrayElements(numbers);
+
+            double avg = AverageValue(numbers);
+            Console.WriteLine("The average value is " + avg);
+        }
+    }
+}
---
+++ programs/CPSC1012-EX09-DavidBergeron/CPSC1012-EX09-DavidBergeron/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace CPSC1012_EX09_DavidBergeron
+{
+    class Program
+    {
+        /*
+         * Enter Grades
+         * populates a passed array with grade values.
+         * Max = Length of Array.
+         * returns how many grades were entered
+         */
+
+        static int GetPositiveInt(string msg)
+        {
+            try
+            {
+                Console.Write(msg);
+                int num = int.Parse(Console.ReadLine());
+                if (num < 0)
+                {
+                    Console.WriteLine("Invalid Input: Enter a non-negative value.");
+                    return GetPositiveInt(msg);
+                }
+                return num;
+            } catch(Exception ex)
+            {
+                Console.WriteLine("Invalid Input: Enter numbers. ");
+                return GetPositiveInt(msg);
+            }
+
+        }
+
+        static int GetPositiveInt(string msg, int max)
+        {
+            int num = GetPositiveInt(msg);
+            if (num > max)
+            {
+                Console.WriteLine($"Invalid Value: Enter number lower than {max}.");
+                GetPositiveInt(msg, max);
+            }
+            return num;
+        }
+
+        static int EnterGrades(int[] grades, int max)
+        {
+            int count = 0;
+            for (int i = 0; i < max; i++)
+            {
+                bool invalidInput = true;
+                while (invalidInput)
+                {
+                    int num = GetPositiveInt("Enter Grade: ");
+                    if (num == 999)
+                    {
+                        return count;
+                    }
+
+                    if (num > 100)
+                    {
+                        Console.WriteLine("Invalid Value: Grade cannot be higher than 100.");
+                    } else
+                    {
+                        invalidInput = false;
+                        grades[i] = num;
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
