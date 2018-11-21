7fa6b9ee39895949e87087f423c27d6910d1f3ec
David Bergeron <dbergeron2@nait.ca>
2018-11-21 10:47:54 -0700

    pushing work on Advanced Portfolio (finished #2), and Read/Write files stuff.

--- programs/CPSC1012-AP-DavidBergeron/CPSC1012-AP-DavidBergeron/Program.cs
+++ programs/CPSC1012-AP-DavidBergeron/CPSC1012-AP-DavidBergeron/Program.cs
@@ -6,8 +6,34 @@ namespace CPSC1012_AP_DavidBergeron
     {
         static void Main(string[] args)
         {
-            var rd = new RoomDimension(3, 2);
-            Console.WriteLine(rd.ToString());
+            double roomLength = GetDouble("Enter the Room Length in Feet: ");
+            double roomWidth  = GetDouble("Enter the Room Length in Feet: ");
+            double carpetCost = GetDouble("Enter the Carpet Cost per Square Foot: ");
+
+            RoomDimension rd = new RoomDimension(roomLength, roomWidth);
+            RoomCarpet carpet = new RoomCarpet(rd, carpetCost);
+            Console.WriteLine(carpet);
+        }
+
+        static double GetDouble(string msg)
+        {
+            try
+            {
+                Console.Write(msg);
+                double num = double.Parse(Console.ReadLine());
+                if (num < 0)
+                {
+                    Console.WriteLine("Invalid Input: Enter a non-negative value.");
+                    return GetDouble(msg);
+                }
+                return num;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Input: Enter numbers. ");
+                return GetDouble(msg);
+            }
+
         }
     }
 }
--- programs/CPSC1012-AP-DavidBergeron/CPSC1012-AP-DavidBergeron/RoomCarpet.cs
+++ programs/CPSC1012-AP-DavidBergeron/CPSC1012-AP-DavidBergeron/RoomCarpet.cs
@@ -4,9 +4,10 @@ using System.Text;
 
 namespace CPSC1012_AP_DavidBergeron
 {
-    class RoomCarpet : RoomDimension
+    class RoomCarpet
     {
         private double _carpetCost;
+        private RoomDimension _roomDimension;
 
         public double Cost
         {
@@ -25,14 +26,23 @@ namespace CPSC1012_AP_DavidBergeron
 
         public RoomCarpet(RoomDimension rd, double cost)
         {
-            this._rd = rd;
+            this._roomDimension = rd;
             this.Cost = cost;
+        }
+
+        public double TotalCost()
+        {
+            return _roomDimension.Area() * _carpetCost;
         }
 
         public string DimensionString
         {
-            get => _rd.ToString();
+            get => _roomDimension.ToString();
         }
 
+        public override string ToString()
+        {
+            return $"The total cost of the carpet is {this.TotalCost():C}";
+        }
     }
 }
